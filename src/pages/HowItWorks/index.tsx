953e6f08f72fc90ea3047ee7f8e636a8f5c1f4a9
jorgel85 <jorge.l@synthesise.me>
2024-10-16 09:19:44 -0400

    update shop page

--- src/pages/HowItWorks/index.tsx
+++ src/pages/HowItWorks/index.tsx
@@ -51,10 +51,16 @@ const HowItWorksPage = () => {
                 color: "#353535",
               }}
             >
-              Get accepted once your account is verified, you’ll be in the
-              running to join one of our exclusive campaigns. If you’re one of
-              the lucky 2 million users in New York, London, or Istanbul, you’ll
-              receive a unique promo code.
+              We’ve tapped into something special, the collective power of
+              people.  We have created a platform where millions of people spend
+              hundreds of dollars during  week long campaigns.  Millions of
+              people, literally spending billions of dollars! That level of
+              spending power gives us enormous negotiating power with brands
+              over price!  It’s so strong that it’s enabled us to negotiate
+              discounts that provide our users with $/€/£ 1 for free for every 1
+              spent up to 500 per campaign. For every campaign we run, there are
+              only limited spaces for users, so be alert if you want to be a
+              lucky one!
             </Typography>
           </Box>
           <Box sx={{ my: 5 }}>
@@ -76,10 +82,12 @@ const HowItWorksPage = () => {
                 color: "#353535",
               }}
             >
-              Get accepted once your account is verified, you’ll be in the
-              running to join one of our exclusive campaigns. If you’re one of
-              the lucky 2 million users in New York, London, or Istanbul, you’ll
-              receive a unique promo code.
+              Simply sign up here and you’ll receive a text and email to verify
+              your account.  Then, if you are one of the 2m users in New York,
+              London or Istanbul accepted,  you’ll receive a unique promo code
+              that will apply a 50% discount on our brands’ websites.  All
+              payments, customer service and fulfilment will take place through
+              the brand site as a  normal shopping experience.
             </Typography>
           </Box>
           <Box sx={{ my: 5 }}>
@@ -101,10 +109,11 @@ const HowItWorksPage = () => {
                 color: "#353535",
               }}
             >
-              Get accepted once your account is verified, you’ll be in the
-              running to join one of our exclusive campaigns. If you’re one of
-              the lucky 2 million users in New York, London, or Istanbul, you’ll
-              receive a unique promo code.
+              We have a new marketing metric -  incremental sales.  We are
+              selling packages of incremental revenue measured  in hundreds of
+              thousands or millions of dollars.  In addition there are short and
+              long term benefits for brand equity and customer relationship
+              management.
             </Typography>
           </Box>
         </Box>
--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -2,9 +2,13 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
+import SearchIcon from "@mui/icons-material/Search";
 
 import dammyPartner from "../../assets/images/dammy_partner.png";
 import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
 
 const ShopPage = () => {
   return (
@@ -16,6 +20,50 @@ const ShopPage = () => {
       }}
     >
       <Container maxWidth="xl">
+        <Box sx={{ mb: 10 }}>
+          <Typography
+            component="p"
+            sx={{
+              fontSize: "16px",
+              fontWeight: 400,
+              color: "#353535",
+              mb: 3,
+            }}
+          >
+            To get the most accurate results when searching for a product, it's
+            important to provide as much detail as possible. Here are some key
+            elements to consider:
+            <br />
+            <strong>
+              Product Name, Brand, Features, Price Range, Purpose, Color, Size
+              and Material.
+            </strong>
+            <br />
+            By providing as much information as possible, you'll increase your
+            chances of finding the perfect product.
+          </Typography>
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              backgroundColor: "#F2F1E7",
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Describe the product you want here..."
+              inputProps={{
+                "aria-label": "Describe the product you want here...",
+              }}
+            />
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Box>
         <Box sx={{ my: 10 }}>
           <Box sx={{ mb: 3, display: "flex", justifyContent: "space-between" }}>
             <Typography
